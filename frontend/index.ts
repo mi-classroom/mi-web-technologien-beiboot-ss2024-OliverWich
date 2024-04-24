6fc4d55702bb759ce88a2f682c2d0d605eaad2a7
OliverWich <kontakt@oliver-wich.de>
2024-04-24 21:26:38 +0200

    fix: better errors

--- frontend/index.ts
+++ frontend/index.ts
@@ -1,4 +1,4 @@
-import {Elysia} from "elysia"
+import {Elysia, error} from "elysia"
 import {staticPlugin} from "@elysiajs/static"
 
 /**
@@ -7,9 +7,13 @@ import {staticPlugin} from "@elysiajs/static"
  * **It causes all not defined routes to be redirected to the index.html of it!**
  */
 export const frontend = new Elysia()
-    .onError(({ code, path, set }) => {
+    .onError(({ code, error }) => {
         if (code === 'NOT_FOUND')
             return Bun.file('./frontend/dist/index.html')
+        else {
+            console.error(error)
+            return error
+        }
     })
     .use(staticPlugin({
         assets: './frontend/dist/',
