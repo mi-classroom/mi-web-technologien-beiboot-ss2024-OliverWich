d66086f8d8722e323b6e33ebe89483a545d68908
OliverWich <kontakt@oliver-wich.de>
2024-05-28 16:54:12 +0200

    [frontend] display basic projects list on dashboard

---
+++ frontend/src/api.ts
@@ -0,0 +1,21 @@
+export function apiCall(url: string, method: string = 'GET', body: any = null) {
+    return fetch(url, {
+        method: method,
+        body: body ? JSON.stringify(body) : undefined,
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(response => response.json())
+}
+
+export function apiGET(url: string) {
+    return apiCall(url, 'GET')
+}
+
+export function apiPOST(url: string, body: any) {
+    return apiCall(url, 'POST', body)
+}
+
+export function getProjects() {
+    return apiGET('/projects')
+}
--- frontend/src/views/DashboardView.vue
+++ frontend/src/views/DashboardView.vue
@@ -1,9 +1,21 @@
 <script setup lang="ts">
+import { getProjects } from '@/api'
+import {ref} from "vue"
+
+const projects = ref<Array<String>>([])
+
+getProjects().then((data) => {
+  console.log(data)
+  projects.value = data as Array<String>
+})
 </script>
 
 <template>
   <main id="dashboard">
-    Overview of all Projects (List)
+    All Projects:<br>
+    {{ projects }}<br>
+    Choose one to go into project view.
+    There you can see all frames, choose slices and fps and render.
     <br/>
     Ability to add Projects
   </main>
